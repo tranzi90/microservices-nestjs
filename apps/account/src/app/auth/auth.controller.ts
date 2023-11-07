a6692f145eaa344ce9f038daaad95309d01f13b4
denis_levchenko <tranzi90@gmail.com>
2023-11-07 23:27:06 +0300

    build account microservice

---
+++ apps/account/src/app/auth/auth.controller.ts
@@ -0,0 +1,24 @@
+import { Body, Controller } from '@nestjs/common';
+import { AccountLogin, AccountRegister } from '../../../../../libs/contracts/src';
+import { RMQRoute, RMQValidate } from 'nestjs-rmq';
+import { AuthService } from './auth.service';
+
+@Controller()
+export class AuthController {
+	constructor(
+		private readonly authService: AuthService
+	) {}
+
+	@RMQValidate()
+	@RMQRoute(AccountRegister.topic)
+	async register(@Body() dto: AccountRegister.Request): Promise<AccountRegister.Response> {
+		return this.authService.register(dto);
+	}
+
+	@RMQValidate()
+	@RMQRoute(AccountLogin.topic)
+	async login(@Body() { email, password }: AccountLogin.Request): Promise<AccountLogin.Response> {
+		const { id } = await this.authService.validateUser(email, password);
+		return this.authService.login(id);
+	}
+}
