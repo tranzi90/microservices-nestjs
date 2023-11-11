004451d8ef53883ecdfc6a8c7fb627586a1320a7
denis_levchenko <tranzi90@gmail.com>
2023-11-11 21:26:04 +0300

    add user queries (CQRS pattern)

--- apps/account/src/app/user/user.commands.ts
+++ apps/account/src/app/user/user.commands.ts
@@ -1,28 +1,22 @@
 import { Body, Controller } from '@nestjs/common';
-import { RMQRoute, RMQValidate } from 'nestjs-rmq';
-import { UserRepository } from './repositories/user.repository';
 import { AccountChangeProfile } from '@purple/contracts';
+import { RMQRoute, RMQValidate } from 'nestjs-rmq';
 import { UserEntity } from './entities/user.entity';
+import { UserRepository } from './repositories/user.repository';
 
 @Controller()
 export class UserCommands {
-  constructor(
-    private readonly userRepository: UserRepository
-  ) {
-  }
+	constructor(private readonly userRepository: UserRepository) {}
 
-  @RMQValidate()
-  @RMQRoute(AccountChangeProfile.topic)
-  async changeProfile(@Body() { user, id }: AccountChangeProfile.Request): Promise<AccountChangeProfile.Response> {
-    const foundUser = await this.userRepository.findUserById(id);
-
-    if (!foundUser)
-      throw new Error('Such user does not exist');
-
-    const userEntity = new UserEntity(foundUser).updateProfile(user.displayName);
-    await this.userRepository.updateUser(userEntity);
-
-    return {};
-  }
-
+	@RMQValidate()
+	@RMQRoute(AccountChangeProfile.topic)
+	async userInfo(@Body() { user, id }: AccountChangeProfile.Request): Promise<AccountChangeProfile.Response> {
+		const existedUser = await this.userRepository.findUserById(id);
+		if (!existedUser) {
+			throw new Error('Такого пользователя не существует');
+		}
+		const userEntity = new UserEntity(existedUser).updateProfile(user.displayName);
+		await this.userRepository.updateUser(userEntity);
+		return {};
+	}
 }
--- libs/contracts/src/lib/account/account.change-profile.ts
+++ libs/contracts/src/lib/account/account.change-profile.ts
@@ -1,19 +1,17 @@
-import {IsString} from 'class-validator';
-import {IUser} from "@purple/interfaces";
+import { IUser } from '@purple/interfaces';
+import { IsString } from 'class-validator';
 
 export namespace AccountChangeProfile {
 	export const topic = 'account.change-profile.command';
 
 	export class Request {
-    @IsString()
-    id: string;
+		@IsString()
+		id: string;
 
 		@IsString()
 		user: Pick<IUser, 'displayName'>;
 	}
 
-	export class Response {
-
-	}
+	export class Response {}
 }
 
